712e4f8c10209ad258b654d493b3bce33cdaaf7f
zoa94 <131690342+zoa94@users.noreply.github.com>
2023-04-27 10:56:25 -0300

    Add files via upload

---
+++ eguadacuia-main/cardapio/img.js
@@ -0,0 +1,9 @@
+let cardapioJson = [
+    {id:1, name:'Caruru', img:'images/caruru.png', price:14.00, sizes:['100g', '530g', '860g'], description:'Experimente o nosso delicioso Caruru Com um sabor único e uma textura incrível, é a opção perfeita para quem procura um prato autêntico e saboroso.'},
+    {id:2, name:'Maniçoba', img:'images/maniçoba.png', price:20.00, sizes:['320g', '530g', '860g'], description:'"Descubra a riqueza de sabores da culinária paraense com nosso saboroso prato de Maniçoba.'},
+    {id:3, name:"Pai-d'égua", img:'images/pai_degua.png', price:25.00, sizes:['320g', '530g', '860g'], description:'Um prato de respeito! massa de caranguejo + arroz e farofa + maniçoba'},
+    {id:4, name:'Tacaranguejo', img:'images/tacaranguejo.png', price:20.00, sizes:['320g', '530g', '860g'], description:'Explore as raízes da culinária do Norte do Brasil com nosso delicioso Tacacá com unhas de Caranguejo.'},
+    {id:5, name:"Trio pai-d'égua", img:'images/trio_pai_dgua.png', price:27.00, sizes:['320g', '530g', '860g'], description:'3 mini porções dos nossos pratos mais deliciosos.'},
+    {id:6, name:'Vatapá', img:'images/vatapa.png', price:15.00, sizes:['320g', '530g', '860g'], description:'Deixe-se levar pelos sabores marcantes da nossa receita de Vatapá'},
+    {id:7, name:'Vataçoba', img:'images/vatasoba.png', price:22.00, sizes:['320g', '530g', '860g'], description:'Deixe-se levar pelos sabores da Amazônia com nossa deliciosa combinação de Vatapá com Maniçoba'}
+];
